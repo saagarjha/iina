4c16de6c74d5e2c0ffb36126cfedf369815bca85
Collider LI <lhc199652@gmail.com>
2017-04-16 21:27:28 +0800

    fix problems after review

--- iina/MainWindowController.swift
+++ iina/MainWindowController.swift
@@ -666,7 +666,7 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
             playerCore.abLoop()
             returnValue = 0
           default:
-            returnValue = playerCore.mpvController.command(raw: kb.rawAction)
+            returnValue = playerCore.mpvController.command(rawString: kb.rawAction)
           }
           // handle return value, display osd if needed
           if returnValue == 0 {
